9fc74416811baaca6aeeab52ebafdd37c0ca31dc
pbertoni <giskard89@gmail.com>
2014-10-20 00:21:46 +0200

    ok per OXM, ma per SP500 non mi spiego il costante fallimento...

--- greedytobia.m
+++ greedytobia.m
@@ -1,41 +1,46 @@
-function [ Ii, x , ZI, i, feasible ] = greedytobia( P, z, p)
+function [ Ii, x , Zi, i, feasible, max_Zi_hist ] = greedytobia( P, z, p)
 %GREEDYTOBIA Gives a greedy solution for the Portfolio Optimization problem.
 %   P is a (TxJ) matrix of relative returns of J securities over T periods (or in T different scenarios, which is equivalent).
 %	z is the minimum return desired by the decision maker
 %	p is the probability with which the z fact will occurr, i.e., Pr( vopt >= z).
 %	pdf is the probability distribution function of the T scenarios.
+%	max_Zi_hist is a vector with the historic data of maximum Zi reached over iterations.
 	[T, ~] = size(P);
 	i = 1;
 	Ii = zeros(1, T);
-	Iprev = zeros(1, T);
+	
 	stop = 0;
 	maxIter = 10;
 	feasible = 0;
-	Zprev = - Inf;
-	Zi = - Inf;
+
+	max_Zi_hist = [];
 	
 	while stop==0 && i < maxIter
+		
+		Iprev = Ii;
 		
 		Fi = zeros(1, T);
 		for t=1:T
 			if Iprev(t) == 0	% t in [1,T] \ I
 				Iprev(t) = 1;
-				[x, ~, flag] = lptobia(P, z, Iprev);
+				[~, ~, flag] = lptobia(P, z, Iprev);
 				Iprev(t) = 0; 
-				if flag == -2
-					fprintf('N(%d) ', t);
-					Zprev = - Inf;
-				else
-					fprintf('F(%d) ', t);
+				if flag ~= -2
 					Fi(t) = 1;
+					if flag ~= 1
+						flag
+					end
 				end
 			end
 		end
-
+		
+		fprintf('\nFi = '), printarray(Fi, 'd'), fprintf('\n')
 		if Fi == zeros(1,T)
-			fprintf('\nstop since Fi = zero vector\n')
 			stop = 1;
-			if Zprev > - Inf
+			[x, ~, flag] = lptobia(P, z, Iprev);
+			if flag == -2
+				feasible = 0;
+			else
 				feasible = 1;
 			end
 		else
@@ -44,44 +49,49 @@ function [ Ii, x , ZI, i, feasible ] = greedytobia( P, z, p)
 			for t=1:T
 				if Fi(t) == 1			% t in Fi
 					if Iprev(t) == 1	% don't need to unify
-						[x, ZI, flag] = lptobia(P, z, Iprev);
+						[x, Zi, flag] = lptobia(P, z, Iprev);
 					else
-						Iprev(t) = 1;		% tmp add of t
-						[x, ZI, flag] = lptobia(P, z, Ii);
-						Ii(t) = 0;		% end of tmp add
+						Iprev(t) = 1;	% tmp add of t
+						[x, Zi, flag] = lptobia(P, z, Iprev);
+						Iprev(t) = 0;		% end of tmp add
 					end
-					
-	%				fprintf('I(%d)', t), printarray(find(I), 'd')
+
 					if flag == -2
-						ZI = - Inf;
+						Zi = - Inf;
 					else
-						if ZI > max_Ztemp
-							fprintf('UP : %f > %f\n', zt, max_zi);
-							Ii(ti) = 0;
+						if Zi > max_Ztemp
+							fprintf('\nUP(%d) : %f > %f', ti, Zi, max_Ztemp);
 							ti = t;
-							Ii(ti) = 1;
-							max_Ztemp = ZI;
-						else
-							fprintf('DW : %f < %f\n', zt, max_zi);
+							max_Ztemp = Zi;
 						end
 					end
 				end
 			end
 			
+			if ti > 0
+				Ii(ti) = 1;
+				fprintf('\nadded %d to Ii = ', ti), printarray(Ii, 'd'), fprintf('\n')
+				max_Zi_hist = [ max_Zi_hist max_Ztemp];
+			end
+			
 			if Ii == ones(1,T)
 				stop = 1;
-				fprintf('\nend of %d: stop since I = one vector\n', i)
+				[x, ~, flag] = lptobia(P, z, Ii);
+				if flag == -2
+					feasible = 0;
+				else
+					feasible = 1;
+				end
 			else
-				fprintf('\nend of %d: I = ', i), printarray(Ii,'d'), disp('')
 				i=i+1;
 			end
 		end
+		fprintf('\n ------ end of %d --------\n', i-1)
 	end
 	
-	if ZI > - Inf
-		fprintf('\ncomputing real sol with Ii = '), printarray(Ii)
-		[x, ZI, ~] = lptobia(P, z, Ii);
-		feasible = 1;
+	[x, Zi, flag] = lptobia(P, z, Ii);
+	if flag == -2
+		fprintf('\n\nTHIS IS NOT POSSIBLE !!!!!')
 	end
 end
 
--- tester.m
+++ tester.m
@@ -1,41 +1,59 @@
 clc, clear, close all
 format short
 
-R = load('OXM.txt'); %SP500.txt
-[Nperiods, Nassets] = size(R);
+maxAssets = 6;		% bound complexity
+maxPeriods = 20;	% bound complexity
+
+R = load('OXM.txt'); %SP500.txt %0XM.txt
+[periods, assets] = size(R);
+
+if periods > maxPeriods
+	R = R(1:maxPeriods, :);
+	fprintf('periods has been reduced from %d to %d to bound complexity.\n\tSorry, buy a faster computer :(\n', periods, maxPeriods);
+	periods = maxPeriods;
+end
+if assets > maxAssets
+	R = R(:, 1:maxAssets);
+	fprintf('assets has been reduced from %d to %d to bound complexity.\n\tSorry, buy a faster computer :(\n', assets, maxAssets);
+	assets = maxAssets;
+end
 
 R(any(isnan(R),2),:) = [];  % handle NaNs
 
+if abs(mean(mean(R))-1) < .1	% sort of detrending to have returns relative not to 0% but to 100%.
+	R = R -1;
+	fprintf('experimental detrend of 1\n');
+end
+
 Rmean = mean(R); % expected mean return
 Rcov = cov(R); % covariance (symm semipos matrix)
-Rstdev = std(R);
 
-Rmad = zeros(Nperiods, Nassets);
-for r=1:Nperiods
+Rmad = zeros(periods, assets);
+for r=1:periods
 	Rmad(r,:) = R(r,:) - Rmean;
 end
 
-fprintf('These tests uses data for %d shares over %d periods.\n', Nassets, Nperiods)
+fprintf('These tests uses data for %d shares over %d periods.\n', assets, periods)
 
 %% PAPAHRISTODOULOUS
 fprintf('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~ PAPAHRISTODOULOUS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 
-f = (1/Nperiods) * ones(Nperiods, 1);
-f = [ f ; zeros(Nassets, 1) ];
+f = (1/periods) * ones(periods, 1);
+f = [ f ; zeros(assets, 1) ];
 
-A = [-eye(Nperiods) Rmad];
+A = [-eye(periods) Rmad];
 b = zeros(size(A,1), 1);
 
-Aeq = [zeros(1, Nperiods) ones(1,Nassets)];
+Aeq = [zeros(1, periods) ones(1,assets)];
 beq = 1;
 
-lb = zeros(Nperiods+Nassets, 1);
-ub = [ Inf(Nperiods,1) ; ones(Nassets,1) ];
+lb = zeros(periods+assets, 1);
+ub = [ Inf(periods,1) ; ones(assets,1) ];
 
 displayOff = optimset('Display','off');
 dumbX0 = zeros(size(f));
 [x,~] = linprog(f,A,b,Aeq,beq,lb,ub,dumbX0,displayOff);
-x_papa = x(Nperiods+1:end);
+x_papa = x(periods+1:end);
 ER_papa = Rmean * x_papa * 100;	% percent
 fprintf('\nx*:\t'), printarray(x_papa,'f')
 fprintf('\nrisk(stdev) = %f | return(mean) = %f %%\n', std(x_papa), ER_papa);
@@ -55,22 +73,25 @@ fprintf('\nx*(max return):\t'), printarray(maxRet,'f')
 fprintf('\nrisk = %f | maximum return = %f %%\n', PortRisk(end), Rmean*maxRet*100);
 
 %% TOBIA
-fprintf('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~ TOBIA (z=1%%) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+z = 0.0001;
+fprintf('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~ TOBIA (z=%d%%) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n', z*100)
 
-z = 0.01;
 p = .5;
-[I,x_tobia,~,~, feasible] = greedytobia(R,z,p);
+[I, x_tobia, ~, i, feasible, max_Zi_hist] = greedytobia(R, z, p);
 if feasible == 1
 	fprintf('\nI*:\t'), printarray(I,'d')
 	fprintf('\nx*:\t'), printarray(x_tobia,'f')
 	ER_tobia = Rmean * x_tobia * 100; % percent
 	fprintf('\nrisk(stdev) = %f | return(mean) = %f %%\n', std(x_tobia), ER_tobia);
+	figure('name', 'Evolution of objective function over iterations');
+	plot(1:length(max_Zi_hist), max_Zi_hist)
 else
-	fprintf('\n This problem is not feasible\n');
+	fprintf('\n This problem is not feasible. Ran %d iterations\n', i);
 end
 
+%% TOBIA OVER Z
 min_z = .001;
-max_z = .01;
+max_z = .1;
 fprintf('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~ TOBIA over z ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 
 trials = 10;
@@ -78,10 +99,12 @@ z_axis = linspace(min_z, max_z, trials);
 risk_axis = zeros(1, trials);
 return_axis = zeros(1, trials);
 iter_axis = zeros(1, trials);
+solutions = zeros(assets, trials);
 tic
 for i = 1 : trials
-	[~,x,~,iter,feasible] = greedytobia(R, z_axis(i), p);
+	[~, x, ~, iter, feasible] = greedytobia(R, z_axis(i), p);
 	if feasible ==1
+		solutions(:,i) = x;
 		risk_axis(i) = std(x);
 		return_axis(i) = Rmean * x;
 		iter_axis(i) = iter;
