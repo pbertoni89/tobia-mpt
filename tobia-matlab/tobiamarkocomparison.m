fcf8b48e426660c5519d5d4aaf1f577a80193ea3
pbertoni <giskard89@gmail.com>
2014-11-29 07:37:03 +0100

    finito io credogit add --all!

--- tobia-matlab/tobiamarkocomparison.m
+++ tobia-matlab/tobiamarkocomparison.m
@@ -58,55 +58,38 @@ fprintf('This test uses data for %d shares over %d periods to compute %d portfol
 
 %% MARKOWITZ
 tic
-[PortRisk, PortReturn, PortWts] = portopt(Rmean, Rcov, edgeSize, [], [], 'algorithm', 'lcprog');
+[MarkoRisk, MarkoReturn, MarkoWts] = portopt(Rmean, Rcov, edgeSize, [], [], 'algorithm', 'lcprog');
 mrkw_time = toc;
-Nports = length(PortRisk);
-minRisk = PortWts(1,:)';
-fprintf('\nMatlab used %f seconds to calculate %d Markowitz frontier portfolios.', mrkw_time, Nports)
+minRisk = MarkoWts(1,:)';
+fprintf('\nMatlab used %f seconds to calculate %d Markowitz frontier portfolios.', mrkw_time, edgeSize)
 fprintf('\nx*(min risk):\t'), printarray(minRisk,'f')
-fprintf('\nminimum risk = %f | return = %f %%\n', PortRisk(1), Rmean*minRisk*100);
-maxRet = PortWts(end, :)';
+fprintf('\nminimum risk = %f | return = %f %%\n', MarkoRisk(1), Rmean*minRisk*100);
+maxRet = MarkoWts(end, :)';
 fprintf('\nx*(max return):\t'), printarray(maxRet,'f')
-fprintf('\nrisk = %f | maximum return = %f %%\n', PortRisk(end), Rmean*maxRet*100);
+fprintf('\nrisk = %f | maximum return = %f %%\n', MarkoRisk(end), Rmean*maxRet*100);
 
-figure('name', 'portfolios');
+figure('name', 'Markowitz assets');
 bar(1:assets, [minRisk, maxRet]);
-strMinRisk = strcat('min risk: ', num2str(PortRisk(1)));
-strMaxReturn = strcat('max ret:', num2str(PortReturn(end)));
+strMinRisk = strcat('min risk: ', num2str(MarkoRisk(1)));
+strMaxReturn = strcat('max ret:', num2str(MarkoReturn(end)));
 legend(strMinRisk,strMaxReturn);
 xlabel('assets'), ylabel('share quota')
 
-save('markowitzReturns.txt', 'PortReturn', '-ascii')
-fprintf('returns to be passed to tobia as z values:\n'); printarray(PortReturn','f')
+save('markowitzReturns.txt', 'MarkoReturn', '-ascii')
+fprintf('returns to be passed to tobia as z values:\n'); printarray(MarkoReturn','f')
 
 %% Build Tobia frontier
-% TobiaWts(:,1) = [0 0 0 0 0 0 0 0 0 0.00026187 0 0 0.96152 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.037908 0 0 0 0 0 0 0 0 0 0 0 0.00030931 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,2) = [0 0 0 0 0 0 0 0 0 0.000092815 0 0 0.93519 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.064383 0 0 0 0 0 0 0 0 0 0 0 0.00033657 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,3) = [0 0 0 0 0 0 0 0 0 0.00012886 0 0 0.91001 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.089389 0 0 0 0 0 0 0 0 0 0 0 0.0004673 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,4) = [0 0 0 0 0 0 0 0 0 0 0 0 0.87567 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12433 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,5) = [0 0 0 0 0 0 0 0 0 0.000093669 0 0 0.85374 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0027781 0 0 0 0 0 0 0 0 0 0.0011498 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.14012 0 0 0 0 0 0 0.0010047 0 0 0 0 0.0011057 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,6) = [0 0 0 0 0 0 0 0 0 0 0 0 0.83591 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.000089582 0 0 0 0 0 0 0 0 0 0 0 0.164 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,7) = [0 0 0 0 0.000098951 0 0 0 0 0 0 0 0.80897 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0013073 0 0 0.014484 0 0 0 0 0 0 0 0 0 0 0 0.17514 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,8) = [0 0 0 0 0 0 0 0 0 0.00067549 0 0 0.7412 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0077158 0 0 0 0 0 0 0 0 0 0 0 0.25041 0 0 0 0 0 0 0 0 0 0];
-% TobiaWts(:,9) = [0 0 0 0 0 0 0 0 0 0 0 0 0.74796 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.017307 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0066105 0 0 0 0 0 0 0 0 0 0 0 0.22287 0 0 0 0 0 0.0052519 0 0 0 0];
-% TobiaWts(:,10)= [0 0 0 0 0 0 0 0 0 0 0 0 0.74305 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.021778 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.22382 0 0.0015573 0 0 0 0.0098006 0 0 0 0];
-% TobiaWts(:,11)= [0 0 0 0 0 0 0 0 0 0 0 0 0.74305 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.021778 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.22382 0 0.0015573 0 0 0 0.0098006 0 0 0 0];
-% TobiaWts(:,12)= [0 0 0 0 0 0 0 0 0 0 0 0 0.74305 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.021778 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.22382 0 0.0015573 0 0 0 0.0098006 0 0 0 0];
-% TobiaWts(:,13)= [0 0 0 0 0 0 0 0 0 0 0 0 0.74305 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.021778 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.22382 0 0.0015573 0 0 0 0.0098006 0 0 0 0];
-% TobiaWts(:,14)= [0 0 0 0 0 0 0 0 0 0 0 0 0.74305 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.021778 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.22382 0 0.0015573 0 0 0 0.0098006 0 0 0 0];
-% TobiaWts(:,15)= [0 0 0 0 0 0 0 0 0 0 0 0 0.74305 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.021778 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.22382 0 0.0015573 0 0 0 0.0098006 0 0 0 0];
-
 % these are computed with p = 0.51 because a higher p would result in unfeasible (integer) problems
 TobiaWts(:,1) = [0 0 0.0024305 0 0 0 0.00071293 0 0 0 0 0 0 0 0 0.00023006 0 0.98224 0 0.0034598 0 0 0 0.00062662 0 0.00048172 0 0.000061495 0 0.001871 0 0 0 0 0 0 0.00038464 0.000055357 0 0 0.0053835 0 0 0 0 0 0 0.000096256 0 0 0.00035758 0.00024068 0 0 0 0 0 0.0012743 0 0 0 0 0 0 0 0 0 0 0 0 0.000095189 0 0 0 0 0 0 0 0 0];
 TobiaWts(:,2) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.96262 0 0 0 0 0 0.004951 0 0 0 0.0032476 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.024116 0 0 0 0 0 0 0 0 0 0 0.0014738 0 0 0.0035934 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
 TobiaWts(:,3) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.92972 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.059018 0 0 0 0 0 0 0 0 0 0 0.0074667 0 0 0.0037981 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
 TobiaWts(:,4) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.91027 0 0 0 0 0 0 0 0.00060422 0 0 0 0 0 0.0013403 0 0 0 0 0 0 0 0 0 0 0 0 0.082281 0 0 0 0 0 0.0055074 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
 TobiaWts(:,5) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.89236 0 0 0 0 0 0 0 0.0041994 0 0.0020984 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.099164 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0020554 0 0 0.00011794 0 0 0 0 0 0];
-TobiaWts(:,6) =  [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8693 0 0 0 0 0 0 0 0.0052789 0 0.0026089 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12281 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-TobiaWts(:,7) = [0 0 0.000053483 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.84647 0 0 0 0 0 0.00082159 0 0.0053252 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.14733 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-TobiaWts(:,8) =[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8218 0 0 0 0 0 0.00095966 0 0.0062076 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.17095 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.000084065 0 0 0 0 0 0 0 0 0 0 0];
-TobiaWts(:,9) =[0 0 0 0 0 0 0 0 0 0 0 0 0 0.00009443 0 0 0 0.78599 0 0 0 0 0 0.00059003 0 0.016039 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.19165 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0056358 0 0 0 0 0 0 0 0 0 0 0];
-TobiaWts(:,10)= X = [0 0 0 0 0 0 0 0.0022915 0 0 0 0 0 0 0 0.0029097 0 0.75454 0 0 0 0 0 0.0034509 0 0.0082623 0 0.0045493 0 0 0 0.00017514 0 0 0 0 0 0 0 0 0 0 0 0 0.21717 0 0 0 0 0 0 0 0 0 0 0.0031958 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.00075295 0 0 0.0026985 0 0 0 0 0 0];
+TobiaWts(:,6) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8693 0 0 0 0 0 0 0 0.0052789 0 0.0026089 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12281 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+TobiaWts(:,7) = [0 0 0.000053483 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.84647 0 0 0 0 0 0.00082159 0 0.0053252 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.14733 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+TobiaWts(:,8) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8218 0 0 0 0 0 0.00095966 0 0.0062076 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.17095 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.000084065 0 0 0 0 0 0 0 0 0 0 0];
+TobiaWts(:,9) = [0 0 0 0 0 0 0 0 0 0 0 0 0 0.00009443 0 0 0 0.78599 0 0 0 0 0 0.00059003 0 0.016039 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.19165 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0056358 0 0 0 0 0 0 0 0 0 0 0];
+TobiaWts(:,10)= [0 0 0 0 0 0 0 0.0022915 0 0 0 0 0 0 0 0.0029097 0 0.75454 0 0 0 0 0 0.0034509 0 0.0082623 0 0.0045493 0 0 0 0.00017514 0 0 0 0 0 0 0 0 0 0 0 0 0.21717 0 0 0 0 0 0 0 0 0 0 0.0031958 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.00075295 0 0 0.0026985 0 0 0 0 0 0];
 
 %% Frontiers
 
@@ -120,4 +103,16 @@ TobiaWts(:,10)= X = [0 0 0 0 0 0 0 0.0022915 0 0 0 0 0 0 0 0.0029097 0 0.75454 0
 TobiaRisk = std(R*TobiaWts);
 TobiaReturn = Rmean*TobiaWts;
 
-plotfrontiercomparison(Rstd, Rmean, PortRisk, PortReturn, TobiaRisk, TobiaReturn);
+plotfrontiercomparison(Rstd, Rmean, MarkoRisk, MarkoReturn, TobiaRisk, TobiaReturn);
+
+figure('name','Comparison of Marko-Tobia returns');
+plot(MarkoReturn, MarkoReturn,'r'), hold on
+plot(MarkoReturn, TobiaReturn), legend('\mu_{Marko}', '\mu{Tobia}')
+
+figure('name','Comparison of Marko-Tobia risks');
+plot(MarkoRisk,'r'), hold on
+plot(TobiaRisk), legend('\sigma_{Marko}', '\sigma{Tobia}')
+
+figure('name','Tobia assets')
+bar(1:assets, TobiaWts);
+xlabel('assets'), ylabel('share quota')
